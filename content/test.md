6038f4347484e162ba4b5f60e7abd8a420742a3e
Talha-Altair <cooltalha27@gmail.com>
2021-12-31 23:04:30 +0530

    Updates for 2021-12-31 23:04:30

---
+++ content/test.md
@@ -0,0 +1,34 @@
+---
+title: "test"
+author: "Altair"
+type: article
+draft: false
+--- 
+
+```python
+print
+```
+
+
+
+
+    <function print>
+
+
+
+
+```python
+a = 10
+a
+```
+
+
+
+
+    10
+
+
+
+### EEEE
+
+[altair](github.com/talha-altair)
--- make.py
+++ make.py
@@ -1,53 +1,51 @@
 # Load libraries
 import os
-import re
-import fileinput
-import sys
-from glob import glob
-import shutil
 import datetime
 
-path = 'content/'
+nb_path = 'notebooks/'
 
-# Find all jupyter notebooks in all content folders
-all_ipynb_files = [os.path.join(root, name)
-                   for root, dirs, files in os.walk(path)
-                       for name in files
-                           if name.endswith((".ipynb"))]
+def main():
 
-# Remove all notebooks from checkpoint folders
-ipynb_files = [ x for x in all_ipynb_files if ".ipynb_checkpoints" not in x ]
+    all_ipynb_files = [os.path.join(root, name)
+                    for root, dirs, files in os.walk(nb_path)
+                        for name in files
+                            if name.endswith((".ipynb"))]
 
-# For each file
-for file in ipynb_files:
-    # Convert into markdown
-    os.system('jupyter nbconvert --to markdown {file}'.format(file=file))
+    ipynb_files = [ x for x in all_ipynb_files if ".ipynb_checkpoints" not in x ]
 
-date_time = datetime.datetime.now()
-author = "Altair"
+    md_files = [ x.replace(".ipynb", ".md") for x in ipynb_files]
 
-md_files = [ x.replace(".ipynb", ".md") for x in ipynb_files]
+    md_files = [ x.replace("notebooks", "content") for x in md_files ]
 
+    md_temp_files = [ "../" + x for x in md_files ]
 
-for single_file in md_files:
+    for nb_file, md_file in zip(ipynb_files, md_temp_files):
+        os.system('jupyter nbconvert --to markdown {nb_file} --output {md_file}'.format(nb_file=nb_file, md_file=md_file))
 
-    title = single_file.split('/')[-1].split('.')[0]
+    date_time = datetime.datetime.now().strftime("%Y-%m-%d")
+    author = "Altair"
 
-    template_metadata = f"""---
+    for single_file in md_files:
+
+        title = single_file.split('/')[-1].split('.')[0]
+
+        template_metadata = f"""---
 title: "{title}"
 author: "{author}"
-date: 2021-07-07T00:00:00-07:00
 type: article
 draft: false
 --- \n
 """
 
-    # Read the file
-    with open(single_file, 'r') as f:
-        content = f.read()
-        new_content = template_metadata + content
-        with open(single_file, "w") as f:
-            f.write(new_content)
+        # Read the file
+        with open(single_file, 'r') as f:
+            content = f.read()
+            new_content = template_metadata + content
+            with open(single_file, "w") as f:
+                f.write(new_content)
         
+if __name__ == "__main__":
+
+    main()
 
 
